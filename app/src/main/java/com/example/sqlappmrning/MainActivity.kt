431f57800b9fa7cab283e1062b8c8d5feb7c99b7
eMobilis <elikibetael4@gmail.com>
2023-04-17 10:13:39 +0300

    sqlitedb.1

--- app/src/main/java/com/example/sqlappmrning/MainActivity.kt
+++ app/src/main/java/com/example/sqlappmrning/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.sqlappmrning
 
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var edtName:EditText
     private lateinit var edtEmail:EditText
     private lateinit var edtID:EditText
+    private lateinit var db:SQLiteDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,10 +26,18 @@ class MainActivity : AppCompatActivity() {
         edtEmail = findViewById(R.id.edtemail)
         edtID = findViewById(R.id.edtid)
 
+        //Create a database called Emobilis
+        db =openOrCreateDatabase("EmobilisDB", Context.MODE_PRIVATE, null)
+        //Create a table inside the database
+        db.execSQL("CREATE TABLE IF NOT EXISTS users(jina VARCHAR,arafa VARCHAR,kitambulisho VARCHAR)")
         btnSave.setOnClickListener {
+            var name_edt = edtName.text.toString().trim()
+            var email_edt = edtEmail.text.toString().trim()
+            var id_edt = edtID.text.toString().trim()
 
         }
         btnView.setOnClickListener {
+
 
         }
         btnDelete.setOnClickListener {
